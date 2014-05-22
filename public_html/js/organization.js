b6588a80402035cb056edd6feb6f648045a07af5
Jawad Rashid <jawadrashid2010@gmail.com>
2014-05-22 15:56:53 +0500

    Main Work Complete. Working on Tweaks

--- public_html/js/organization.js
+++ public_html/js/organization.js
@@ -6,10 +6,10 @@
 
 
 var loadOrganizationData = function() {
-    var dataForRect = []; 
+    dataForRect = []; 
     processedData = {"types":{"Fiduciary": [], "C-Level": [],"OperationalDelivery": [], "Facilities": [], "highly": []},  
-                    "project":[], "connection": [], "notes": [] };
-    var svg;
+                    "project":[], "connection": [], "notes": [], "relations": [] };
+    
      var w = 1200, h = 700;
      var textHeight = 20;
     d3.json("http://connectionplanner.apiary-mock.com/orgchart/1", function(error, json) {
@@ -34,31 +34,208 @@ var loadOrganizationData = function() {
         
         drawRectangles();
         drawNames();
+        drawLegend();
+        drawRelations();
+    };
+    
+    var drawRelations = function() {
+        var lineGroup = svg.append("svg:g")
+                .attr("id", "lines");
         
+        for(i in processedData.relations) {
+            var rel = processedData.relations[i];
+            var oursID = replaceSpace(rel.Us);
+            var theirsID = replaceSpace(rel.Them);
+            var ours = d3.select("#"+oursID);
+            var theirs = d3.select("#"+theirsID);
+            var oursPoint = {x:0, y:0};
+            var oursData = d3.select("#"+oursID).data()[0];
+            var theirsData = d3.select("#"+theirsID).data()[0];
+            oursPoint.x = toInt(oursData.x) + toInt($("#"+oursID).width());
+            oursPoint.y = toInt(oursData.y) + textHeight * (toInt(ours.attr("index"))+2) - textHeight/4;
+           
+            console.log(oursPoint);
+            var theirsPoint = {x:0, y:0};
+            theirsPoint.x = toInt(theirsData.x);
+            theirsPoint.y = toInt(theirsData.y) + textHeight * (toInt(theirs.attr("index"))+2) - textHeight/4;
+
+           
+            
+            var line = lineGroup.append("line")
+                    .attr("x1", oursPoint.x)
+                    .attr("y1", oursPoint.y)
+                    .attr("x2", theirsPoint.x)
+                    .attr("y2", theirsPoint.y)
+                    .attr("style", "stroke-width:1;stroke:black;");
+            if(rel.Strength === "Strong") {
+                line.attr("stroke-dasharray", "5, 5");
+            }
+                    
+           
+        }
+        
+        var fudiciaryBottomPoint = middleBottomPoint("Fiduciary");
+        var cLevelTopPoint = middleTopPoint("C-Level");
+        lineGroup.append("line")
+                    .attr("x1", fudiciaryBottomPoint.x)
+                    .attr("y1", fudiciaryBottomPoint.y)
+                    .attr("x2", cLevelTopPoint.x)
+                    .attr("y2", cLevelTopPoint.y)
+                    .attr("style", "stroke-width:1;stroke:black;");
+            
+        var cLevelBottomPoint = middleBottomPoint("C-Level");
+        var facTopPoint = middleTopPoint("Facilities");
+        lineGroup.append("line")
+                    .attr("x1", cLevelBottomPoint.x)
+                    .attr("y1", cLevelBottomPoint.y)
+                    .attr("x2", facTopPoint.x)
+                    .attr("y2", facTopPoint.y)
+                    .attr("style", "stroke-width:1;stroke:black;");
+            
+        var highlyPoint = middleRightPoint("highly");
+        var operationalPoint = middleLeftPoint("OperationalDelivery");
+        lineGroup.append("line")
+                    .attr("x1", highlyPoint.x)
+                    .attr("y1", highlyPoint.y)
+                    .attr("x2", operationalPoint.x)
+                    .attr("y2", operationalPoint.y)
+                    .attr("style", "stroke-width:1;stroke:black;");
+    };
+    
+    var middleBottomPoint = function(id) {
+        var rect = d3.select("#"+id).select("rect");
+        
+        return {x: toInt(rect.attr("x")) + toInt(rect.attr("width")/2), y: toInt(rect.attr("y")) + toInt(rect.attr("height"))};
+    };
+    
+    var middleTopPoint = function(id) {
+        var rect = d3.select("#"+id).select("rect");
+        return {x: toInt(rect.attr("x")) + toInt(rect.attr("width")/2), y: toInt(rect.attr("y"))};
+    };
+    
+    var middleRightPoint = function(id) {
+        var rect = d3.select("#"+id).select("rect");
+        return {x: toInt(rect.attr("x")) + toInt(rect.attr("width")), y: toInt(rect.attr("y")) + toInt(rect.attr("height"))/2};
+    };
+    
+    var middleLeftPoint = function(id) {
+        var rect = d3.select("#"+id).select("rect");
+        return {x: toInt(rect.attr("x")), y: toInt(rect.attr("y")) + toInt(rect.attr("height"))/2};
+    };
+    
+    var toInt = function(input) {
+      return parseInt(input);  
+    };
+    
+    var drawLegend = function() {
+        var legend = d3.select("#legend");
+        var legendRect = legend.select("rect");
+        var legendText = legend.select("text");
+        legendText.append("tspan")
+                .text("\tDecision Maker")
+                .attr("class", "DecisionMaker personname")
+                .attr("x", legendRect.attr("x"))
+                .attr("dy", textHeight)
+                .attr("text-anchor", "start");
+        legendText.append("tspan")
+                .text("\tA person with decision making")
+                .attr("class", "personname");
+        legendText.append("tspan")
+                .text("\tpower who can advance your goals")
+                .attr("class", "personname")
+                .attr("x", legendRect.attr("x"))
+                .attr("dy", textHeight)
+                .attr("text-anchor", "start");
+        legendText.append("tspan")
+                .text("\t")
+                .attr("x", legendRect.attr("x"))
+                .attr("dy", textHeight)
+                .attr("text-anchor", "start");
+        
+        legendText.append("tspan")
+                .text("\tNavigator")
+                .attr("class", "Navigator personname")
+                .attr("x", legendRect.attr("x"))
+                .attr("dy", textHeight)
+                .attr("text-anchor", "start");
+        legendText.append("tspan")
+                .text("\tA person who can guide you through")
+                .attr("class", "personname");
+        legendText.append("tspan")
+                .text("\tthe organization.")
+                .attr("class", "personname")
+                .attr("x", legendRect.attr("x"))
+                .attr("dy", textHeight)
+                .attr("text-anchor", "start");
+        legendText.append("tspan")
+                .text("\t")
+                .attr("x", legendRect.attr("x"))
+                .attr("dy", textHeight)
+                .attr("text-anchor", "start");
+        
+        legendText.append("tspan")
+                .text("\tChampion")
+                .attr("class", "Champion personname")
+                .attr("x", legendRect.attr("x"))
+                .attr("dy", textHeight)
+                .attr("text-anchor", "start");
+        legendText.append("tspan")
+                .text("\tA person with leadership level influence")
+                .attr("class", "personname");
+        legendText.append("tspan")
+                .text("\twho wants you to win.")
+                .attr("class", "personname")
+                .attr("x", legendRect.attr("x"))
+                .attr("dy", textHeight)
+                .attr("text-anchor", "start");
+        legendText.append("tspan")
+                .text("\t")
+                .attr("x", legendRect.attr("x"))
+                .attr("dy", textHeight)
+                .attr("text-anchor", "start");
+                
     };
     
     var processData = function() {
       var people = organizationData.People;
-      for(index in people) {
+      for(var index in people) {
           var thisPerson = people[index];
-          var type = thisPerson.Type.replace(/\s+/g, '');
+          var type = replaceSpace(thisPerson.Type);
           processedData.types[type].push(thisPerson);
       }  
       
       processedData.project = organizationData.Partners.slice(0);
+      processedData.notes = organizationData.Notes;
+      
+      var ourNames = [];
+      var relations = organizationData.Relationships;
+      processedData.relations = relations;
+      for(var i in relations) {
+          var thisRel = relations[i];
+          if(ourNames.indexOf(thisRel.Us) === -1) {
+              ourNames.push(thisRel.Us);
+          }
+      }
+      processedData.connection = ourNames;
     };
     
     var drawNames = function() {
-        var key;
-        for(key in processedData.types) {
+        for(var key in processedData.types) {
             var arr = processedData.types[key];
             if(arr.length > 0) {
                   var rect = d3.select("#"+key).select("rect");
                   var startingX = rect.attr("x");
-                  var startingY = rect.attr("y") + textHeight * 2;
-                  for(eachIndex in arr) {
+                  for(var eachIndex in arr) {
                       var thisName = arr[eachIndex];
-                      var thisId = thisName.Name.replace(/\s+/g, '');
+                      var thisId = replaceSpace(thisName.Name);
+                      var additionalClass = "";
+                      if(thisName.Role === "Champion" ) {
+                          additionalClass = " Champion";
+                      } else if(thisName.Role === "Decision Maker") {
+                          additionalClass = " DecisionMaker";;
+                      } else if(thisName.Role === "Navigator") {
+                          additionalClass = " Navigator";
+                      }
                       d3.select("#"+key)
                           .select("text")
                           .append("tspan")
@@ -67,46 +244,102 @@ var loadOrganizationData = function() {
                           .attr("x",startingX)
                           .attr("id", thisId)
                           .attr("text-anchor", "start")
-                          .attr("class", "personname");
+                          .attr("class", "personname"+additionalClass)
+                          .attr("index", eachIndex);
+                      
                   }
                   
             }
         }
         
-//        var 
-//        for(key in processedData.Partners) {
-//            var thisPartner = processedData.Partners[key];
-//            
-//        }
+        var connection = d3.select("#connection");
+        var connText = connection.select("text");
+        var connRect = connection.select("rect");
+        for(var ourIndex in processedData.connection) {
+            var ourPerson = processedData.connection[ourIndex];
+            var thisPersonId = replaceSpace(ourPerson);
+            connText.append("tspan")
+                .text("\t" + ourPerson)
+                .attr("dy", textHeight)
+                .attr("x",connRect.attr("x"))
+                .attr("id", thisPersonId)
+                .attr("text-anchor", "start")
+                .attr("class", "personname")
+                .attr("index", ourIndex);
+        }
+         
+          var initX = d3.select("#project").select("text").attr("x");
+          d3.select("#project text")
+                  .append("tspan")
+                  .text("Project Participants")
+                  .attr("dy", textHeight)
+                  .attr("x",initX);
+          
+          var rectX = d3.select("#project").select("rect").attr("x");
+          var project = d3.select("#project text");
+          for(key in processedData.project) {
+              var thisPartner = processedData.project[key];
+              project.append("tspan")
+                      .text("\t" + thisPartner)
+                      .attr("dy", textHeight)
+                      .attr("x", rectX)
+                      .attr("id", replaceSpace(thisPartner))
+                      .attr("text-anchor", "start")
+                      .attr("class", "personname");
+              ;
+          }
+          
+          var notesRectangle = d3.select("#notes").select("rect").attr("x");
+          var notesLines = processedData.notes.split("</p><p>");
+          var notesNode = d3.select("#notes text");
+          for(eachIndex in notesLines) {
+              var thisNote = notesLines[eachIndex];
+              thisNote = thisNote.replace(/<\/?p>/g, "");
+              notesNode.append("tspan")
+                    .text("\t" + thisNote)
+                    .attr("dy", textHeight)
+                    .attr("x", notesRectangle)
+                    .attr("text-anchor", "start")
+                    .attr("class", "personname");
+          }
+//          d3.select("#notes text").append("tspan")
+//                  .text("\t");
+    };
+    
+    var replaceSpace = function(input) {
+        input = input.replace("'", "");
+        return input.replace(/\s+/g, '');
     };
     
     var drawRectangles = function() {
-      for(index in dataForRect)  {
-          var eachRect = dataForRect[index];
-          
-          var group = svg.append("svg:g")
-                  .attr("id", eachRect.id);
-          
-          group.append("rect")
-                  .attr("width", eachRect.width)
-                  .attr("height", eachRect.height)
-                  .attr("x", eachRect.x)
-                  .attr("y", eachRect.y)
-                  .attr("rx", 20)
-                  .attr("ry", 20)
-                  .attr("fill", "white")
-                  .attr("stroke", "black");
-          
-          group.append("text")
-                  .attr("width", eachRect.width)
-                  .attr("height", textHeight)
-                  .attr("x", (eachRect.width)/2 + eachRect.x)
-                  .attr("y", eachRect.y + 20)
-                  .attr("text-anchor", "middle")
-                  .attr("class", "heading")
-                  .append("tspan")
-                  .text(eachRect.name);
-      } 
+       
+        
+        var groups = svg.selectAll("g")
+                .data(dataForRect)
+                .enter()
+                .append("g")
+                .attr("id", function(d, index) {return d.id});
+        
+        groups.append("rect")
+            .attr("width", function(d) { return d.width; })
+            .attr("height", function(d) { return d.height; })
+            .attr("x", function(d) { return d.x; })
+            .attr("y", function(d) { return d.y; })
+            .attr("rx", 20)
+            .attr("ry", 20)
+            .attr("fill", "white")
+            .attr("stroke", "black");
+    
+        groups.append("text")
+            .attr("width", function(d) { return d.width; })
+            .attr("height", textHeight)
+            .attr("x", function(d) { return (d.width)/2 + d.x; })
+            .attr("y", function(d) { return d.y + 20; })
+            .attr("text-anchor", "middle")
+            .attr("class", "heading")
+            .append("tspan")
+            .text(function(d) {return d.name; });
+        
     };
     
     var createDataArray = function() {
@@ -119,7 +352,7 @@ var loadOrganizationData = function() {
        
         
         //Project Delivery 0
-        makeDataObject("Project Delivery Partners", "project" , initialX, initialY, smallBoxWidth , smallBoxHeight );
+        makeDataObject("Project Delivery Partners", "project" , initialX, initialY, smallBoxWidth * 1.6 , smallBoxHeight );
         
         //Fudiciary 1
         var projectDeliveryRight = getRightCordinateForRect(0);
@@ -151,7 +384,7 @@ var loadOrganizationData = function() {
         
         //Legend
         var facilitiesTopRight = getRightCordinateForRect(7);
-        makeDataObject("Legend", "legend" , facilitiesTopRight.x + gapBetweenBoxes, facilitiesTopRight.y, smallBoxWidth, smallBoxHeight * 2);
+        makeDataObject("Legend", "legend" , facilitiesTopRight.x + gapBetweenBoxes, facilitiesTopRight.y, smallBoxWidth * 1.5, smallBoxHeight * 2);
     };
     
     function getRightCordinateForRect(index) {
